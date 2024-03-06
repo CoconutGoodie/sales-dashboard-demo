85a3959a5d9b0c8f3956ea806cf39d8e2bffe0a5
Anılcan Metinyurt <igoodie@programmer.net>
2024-03-07 01:32:33 +0300

    feat: finish rendering SalesActivity widget

--- src/components/SalesActivityWidget.tsx
+++ src/components/SalesActivityWidget.tsx
@@ -1,49 +1,86 @@
-import { ComponentRef, useEffect, useRef, useState } from "react";
+import {
+  ComponentRef,
+  useEffect,
+  useId,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { MathUtils } from "@src/util/math.utils";
 import CircleArrowUp02 from "@src/assets/icons/hugeicons/circle-arrow-up02.svg?component";
 
 import styles from "./SalesActivityWidget.module.scss";
 
 export const SalesActivityWidget = () => {
-  // const pseudoData = [100, 200, 150, 400, 600, 500, 800, 900];
-  const pseudoData = Array.from({ length: 8 }).map(
-    (_, i) => 50 + Math.random() * 100 * (i + 1)
-  );
+  const pseudoData = useMemo(() => {
+    return Array.from({ length: 8 }).map(
+      (_, i) => 50 + Math.random() * 1000 * (i + 1)
+    );
+  }, []);
 
   const rootRef = useRef<ComponentRef<"div">>(null);
+  const [boundingBox, setBoundingBox] = useState<DOMRect>();
 
-  const [points, setPoints] = useState<MathUtils.Vector2[]>([]);
+  const strokeColorId = useId();
+  const fillColorId = useId();
 
   const n2 = pseudoData.at(-2)!;
   const n1 = pseudoData.at(-1)!;
   const percentageDifference = 100 * ((n1 - n2) / ((n1 + n2) / 2));
 
-  useEffect(() => {
-    if (!rootRef.current) return;
+  const total = pseudoData.reduce((sum, data) => sum + data, 0);
 
-    const boundingBox = rootRef.current.getBoundingClientRect();
+  const vertices = useMemo<MathUtils.Vector2[]>(() => {
+    if (!boundingBox) return [];
 
     const maxValue = pseudoData.reduce(
       (max, data) => Math.max(max, data),
       -Infinity
     );
 
-    setPoints(
-      pseudoData.map((data, index) => [
-        MathUtils.mapRange(
-          index,
-          [0, pseudoData.length - 1],
-          [0, boundingBox.width]
-        ),
-        MathUtils.mapRange(data, [0, maxValue], [0, boundingBox.height * 0.6]),
-      ])
-    );
+    return pseudoData.map((data, index) => [
+      MathUtils.mapRange(
+        index,
+        [0, pseudoData.length - 1],
+        [0, boundingBox.width]
+      ),
+      MathUtils.mapRange(data, [0, maxValue], [0, boundingBox.height * 0.6]),
+    ]);
+  }, [boundingBox]);
+
+  useEffect(() => {
+    if (!rootRef.current) return;
+    setBoundingBox(rootRef.current.getBoundingClientRect());
   }, []);
+
+  function generatePath(points: MathUtils.Vector2[]) {
+    if (points.length < 2) {
+      return "";
+    }
+
+    let d = `M 0 0`;
+
+    for (let i = 0; i < points.length - 1; i++) {
+      const xc = (points[i][0] + points[i + 1][0]) / 2;
+      const yc = (points[i][1] + points[i + 1][1]) / 2;
+      d += ` Q ${points[i][0]} ${points[i][1]} ${xc} ${yc}`;
+    }
+
+    d += ` T ${points[points.length - 1][0]} ${points[points.length - 1][1]}`;
+    d += ` L ${points[points.length - 1][0] + 100} 0`;
+
+    return d;
+  }
 
   return (
     <div ref={rootRef} className={styles.widget}>
       <h1>Sales Activity</h1>
-      <h2>${pseudoData.reduce((sum, data) => sum + data, 0).toFixed(2)}</h2>
+      <h2>
+        {new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(total)}
+      </h2>
       <h3>
         <CircleArrowUp02
           width={30}
@@ -56,14 +93,36 @@ export const SalesActivityWidget = () => {
       <svg>
         <g transform="translate(0,300) scale(1,-1)">
           <path
-            d={points.reduce((d, point, index) => {
-              if (index === 0) {
-                return `M ${point[0]},${point[1]}`;
-              }
-              return `${d} L ${point[0]},${point[1]}`;
-            }, "")}
+            d={generatePath(vertices)}
+            stroke={`url(#${strokeColorId})`}
+            fill={`url(#${fillColorId})`}
           />
         </g>
+
+        <defs>
+          <linearGradient
+            id={strokeColorId}
+            x1="319"
+            y1="-25"
+            x2="19"
+            y2="206"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#BFD1FF" />
+            <stop offset="1" stop-color="#4676F0" />
+          </linearGradient>
+          <linearGradient
+            id={fillColorId}
+            x1={400 - 203}
+            y1={vertices.reduce((max, v) => Math.max(max, v[1]), -Infinity)}
+            x2={400 - 188}
+            y2={200 - 186}
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#0F4EF0" />
+            <stop offset="1" stop-color="#3167F2" />
+          </linearGradient>
+        </defs>
       </svg>
     </div>
   );
