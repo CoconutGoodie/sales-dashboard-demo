dd96f5f244bedb40c8c9ca7188d7ed88fa01c8a9
Anılcan Metinyurt <igoodie@programmer.net>
2024-03-07 03:50:14 +0300

    feat: add title and button to Expenses widget

--- src/components/widgets/ExpensesWidget.tsx
+++ src/components/widgets/ExpensesWidget.tsx
@@ -1,6 +1,9 @@
 import { MathUtils } from "@src/util/math.utils";
 import { ComponentRef, useRef } from "react";
 
+import styles from "./ExpensesWidget.module.scss";
+import { Button } from "@src/components/Button";
+
 export const ExpensesWidget = () => {
   const svgRef = useRef<ComponentRef<"svg">>(null);
 
@@ -18,6 +21,7 @@ export const ExpensesWidget = () => {
         transform={`translate(${width / 2},${height / 2}) rotate(${fromAngle})`}
       >
         <path
+          className={styles.arc}
           d={`M ${radius} 0 A ${radius} ${radius} 0 0 1 ${x} ${y}`}
           stroke={color}
           strokeWidth={9}
@@ -29,20 +33,17 @@ export const ExpensesWidget = () => {
   };
 
   return (
-    <div
-      style={{
-        background: "#fff",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
+    <div className={styles.widget}>
+      <h1>Expenses</h1>
+
       <svg ref={svgRef} width={130} height={130}>
         {generateArc(0, 90, "#3167F2")}
         {generateArc(105, 200, "#6D98FF")}
         {generateArc(215, 250, "#E3EAFD")}
         {generateArc(265, -15, "#B9CDFD")}
       </svg>
+
+      <Button className={styles.button}>Details</Button>
     </div>
   );
 };
